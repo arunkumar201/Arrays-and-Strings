d2fcb2e1893808da08a30ca40d874e8d78bb0d36
Arun Kumar <arunprajapati9825@gmail.com>
2024-01-11 07:50:24 +0530

    updated

--- java/src/com/company/BuyAndSell.java
+++ java/src/com/company/BuyAndSell.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import static java.lang.Math.max;
+
 public class BuyAndSell {
 
     static int maxProfit(int[] a) {
@@ -7,28 +9,29 @@ public class BuyAndSell {
         int max1 = Integer.MIN_VALUE;
         int x = 0;
         int i = 0;
-        for (i = 0; i < n; i++){
+        for (i = 0; i < n; i++) {
             x = a[i];
-        for (int j = i + 1; j <=n - 1; j++) {
-            int diff = a[j] - x;
-            if (diff >=x && diff > max1) {
-                max1 = Math.max(max1, diff);
+            for (int j = i + 1; j <= n - 1; j++) {
+                int diff = a[j] - x;
+                if (diff >= x && diff > max1) {
+                    max1 = max(max1, diff);
 
+                }
             }
         }
-       }
-        if(max1<0){
+        if (max1 < 0) {
             return 0;
         }
         return max1;
 
     }
+
     public static void main(String[] args) {
 //        int []a={7,1,5,3,6,4};
-        int []a={1,2};
-        int n=a.length;
-       int ans= maxProfit(a);
-        System.out.println("Max Profit:"+ans);
+        int[] a = {1, 2};
+        int n = a.length;
+        int ans = maxProfit(a);
+        System.out.println("Max Profit:" + ans);
 
     }
 }
